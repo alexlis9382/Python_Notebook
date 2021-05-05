6ce7571e0ad1f5393c8e8a7f9be965863a2657f7
alexlis9382 <42222260+alexlis9382@users.noreply.github.com>
2021-05-05 10:19:16 +0800

    Create 1_Note.py

---
+++ 1_CrashCourse/1_Note.py
@@ -0,0 +1,5 @@
+#Use string format to fill in blanks with variable
+'My nubmer is {} and my name is {}'.format(num, name)
+
+#This makes it more flexible
+'My nubmer is {a} and my name is {b}'.format(b=num, a=name)
